f0bcd45c4494f6c649313ec121ef0061ff4d99eb
Manoj Paramsetti <paramsetti.manoj@gmail.com>
2022-01-27 09:35:27 +0530

    Add: Create about page

---
+++ src/routes/About.jsx
@@ -0,0 +1,108 @@
+import discordLogo from '../images/clyde_blurple.png';
+import fLogo from '../images/f_logo.png';
+import instagramLogo from '../images/instagram.png';
+import linkedinLogo from '../images/in.png';
+
+import '../App.css';
+
+function About() {
+    return (
+        <div className="About">
+            <div className='navbar'>
+                <div className='navbar--container'>
+                    <div className='navbar--left'>
+                        <img src="https://avatars.githubusercontent.com/u/58457318?s=200&v=4" height={46}
+                            style={{
+                                borderRadius: "100%"
+                            }}
+                        ></img>
+                    </div>
+                    <div className='navbar--right'>
+                        <ul>
+                            <li style={{color: "#FFF"}}>About<div className='underline'></div></li>
+                            <li>Events<div className='underline hover-show'></div></li>
+                            <li>Gallery<div className='underline hover-show'></div></li>
+                            <li className='register'>Register</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='upcoming--event'>
+                <div className='upcoming--container'>
+                    <div className='upcoming--poster'></div>
+                    <div className='upcoming-poster-content'>
+                        <div className='upcoming-event-label'>
+                            Upcoming{'\n\n'}Event
+                        </div>
+                        <div className='upcoming-event-title'>
+                            Git & Github
+                        </div>
+                        <div className='upcoming-events'>
+                            Enim reprehenderit do ut labore ad. Anim aliquip
+                            deserunt culpa occaecat.Nulla velit tempor ea sit occaecat quis eiusmod Lorem voluptate pariatur eu consectetur laboris officia. Fugiat culpa tempor elit consectetur reprehenderit tempor exercitation nisi duis elit nostrud quis. Ex in laborum est exercitation. Nulla sint consectetur enim dolor est ut exercitation incididunt. Id amet ut excepteur commodo fugiat culpa sunt laboris proident. Nostrud fugiat commodo eiusmod proident sit proident cillum. Aliqua dolor consequat ipsum officia nulla officia.
+                        </div>
+                        <br />
+                        <br />
+                        <a className='RSVP'>RSVP HERE</a>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className='joinUs'>
+                <div className='joinUs--container'>
+                    <div className='joinUs--discord'>
+                        <div className='discord--logo'>
+                            <img src={discordLogo} width={'50%'}></img>
+                        </div>
+                        <div className='discord--link'>
+                            <p className='discord-text'>
+                                Join in our Discord Community
+                            </p>
+                            <a href='https://bit.ly/mclub-s4' target= {'_blank'} >Click here to join</a>
+                        </div>
+                    </div>
+                    <div className='followUs-links'>
+                        Follow us on:
+                        <img src={fLogo} height={'35px'}></img>
+                        <img src={instagramLogo} height={'35px'}></img>
+                        <img src={linkedinLogo} height={'35px'}></img>
+                    </div>
+                </div>
+            </div>
+            <div className='aboutClub'>
+                <div className='aboutClub--container'>
+                    <h1>
+                        About Club
+                    </h1>
+                    <p>
+                        Sunt elit laborum ullamco excepteur. Deserunt ad excepteur tempor id nostrud ex dolor ex cupidatat. Culpa dolor ex duis ipsum commodo dolore incididunt et sit eiusmod nulla nulla. Reprehenderit veniam laboris id officia nulla laborum mollit consectetur ut ut consectetur elit duis.
+                        <br></br>
+                        <br></br>
+                        Ullamco elit enim sunt magna labore dolor. Nisi non do dolor amet veniam do fugiat commodo nisi eu eiusmod velit velit. Lorem deserunt aliqua aliquip ipsum nostrud Lorem sunt quis eu nulla incididunt elit labore. Ipsum ullamco et elit nisi aliqua consequat. Ut consequat ut aute sint proident sunt cillum.
+                        Ullamco elit enim sunt magna labore dolor. Nisi non do dolor amet veniam do fugiat commodo nisi eu eiusmod velit velit. Lorem deserunt aliqua aliquip ipsum nostrud Lorem sunt quis eu nulla incididunt elit labore. Ipsum ullamco et elit nisi aliqua consequat. Ut consequat ut aute sint proident sunt cillum.
+                        <br></br>
+                        <br></br>
+                        Laboris aute proident eu sunt do magna reprehenderit ea nisi. Proident tempor magna commodo reprehenderit dolor duis in id eiusmod adipisicing eiusmod voluptate. Duis sunt elit adipisicing eu cupidatat officia culpa reprehenderit. Do ea laboris reprehenderit amet ea amet culpa nisi et nisi.
+                    </p>
+                </div>
+            </div>
+            <div className='coreTeam'>
+                <div className='coreTeam--container'>
+                    <h1>Core Team</h1>
+                    <div className='Profiles'>
+                        {
+                            [1,2,3,4,5,6,7,8,7,8,9,10].map((_, i) => <div className='Profile'>
+                            <img src="https://www.sarojhospital.com/images/testimonials/dummy-profile.png" alt="profile" srcset="" />
+                                <b>Name</b>Role
+                            </div>
+                        )
+                        }
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
