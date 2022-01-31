555b737ef049a2ba50d616f3c8574adb9aa7d51e
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2022-01-31 14:23:10 +0530

    Add: Files for events page

---
+++ src/routes/Events.jsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/navbar';
+import '../stylesheets/events.css'
+function Gallery() {
+    return(
+        <div>
+            <NavBar page='Events' />
+            {/*Paste the events code after this line, no need to provide navbar because it is added before this line and remove this line before contributing*/}
+        </div>
+    );
+}
+
+export default Gallery;
