df2cf1f04b313e8b1684c067c81da379389717de
Manoj Paramsetti <39455174+Manoj-Paramsetti@users.noreply.github.com>
2022-02-01 14:15:50 +0530

    Merging events branch to main branch (#9)
    * Add: Files for events page
    
    * Feat: Create Events Page (#8)
    
    * Fix: function name on events.jsx file
    
    Co-authored-by: Nannan <nanna7077@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from './routes/About';
 import Gallery from './routes/Gallery';
+import Events from './routes/Events';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route exact path = "/" element = { <About /> }/>   
                 <Route exact path = "/gallery" element = { <Gallery /> }/>   
+                <Route exact path = "/events" element = { <Events /> }/>   
             </Routes>
         </div>
     );
---
+++ src/routes/Events.jsx
@@ -0,0 +1,31 @@
+import NavBar from '../components/navbar';
+import '../stylesheets/events.css'
+
+function Events() {
+    return(
+        <div>
+            <NavBar page='Events' />
+            <h1 className="title">Events</h1>
+            <div className="event-buttons">
+                <div className="grey-button">Upcoming Events</div>
+                <div className="grey-button">Post Events</div>
+                <div className="grey-button">All Events</div>
+            </div>
+            {
+            [1,2,3,4,5,6,7,8,7,8,9,10].map((_, i) =>
+                <div className="event-cards">
+                    <div className="event-card">
+                        <img className="event-card-image" src="https://placekitten.com/500/500" alt="Event"/>
+                        <div className="event-card-content">
+                            <div className="event-card-title">Event Name <span className="event-card-title-id">#0034</span></div>
+                            <div className="event-card-description">Some Random Text</div>
+                            <div className="event-card-registerbutton">Register</div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Events;
